3bcd8c85b673ad27b59035844903deae7f4e37af
Konstantin <konstantin.writing.code@gmail.com>
2016-12-08 22:01:47 +0300

    (new) Create grain implementations project

---
+++ CaseGrains/CaseGrain.cs
@@ -0,0 +1,15 @@
+﻿namespace CaseGrains
+{
+    using System;
+    using System.Threading.Tasks;
+    using Interfaces;
+    using Orleans;
+
+    public class CaseGrain : Grain, ICase
+    {
+        public Task<string> RunTest(string args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
