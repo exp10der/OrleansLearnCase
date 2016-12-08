2b64dbb4b4190d40fe4b78ce8f2cee16d05659b0
Konstantin <konstantin.writing.code@gmail.com>
2016-12-08 22:20:13 +0300

    (new) Create server silo host

---
+++ SiloHost/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace SiloHost
+{
+    using System;
+    using Orleans.Runtime;
+    using Orleans.Runtime.Configuration;
+    using Orleans.Runtime.Host;
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            CreateServer();
+
+            Console.ReadKey();
+        }
+
+        private static void CreateServer()
+        {
+            var config = ClusterConfiguration.LocalhostPrimarySilo();
+            config.AddMemoryStorageProvider();
+
+            var host = new SiloHost("Test", config);
+            host.LoadOrleansConfig();
+
+            try
+            {
+                host.InitializeOrleansSilo();
+
+                if (host.StartOrleansSilo())
+                    Console.WriteLine($"Successfully started Orleans silo '{host.Name}' as a {host.Type} node.");
+                else
+                    throw new OrleansException($"Failed to start Orleans silo '{host.Name}' as a {host.Type} node.");
+            }
+            catch (Exception e)
+            {
+                host.ReportStartupError(e);
+            }
+        }
+    }
+}
