a3a69da53f9293b7f6851044b41fef5622b83f2c
Konstantin <konstantin.writing.code@gmail.com>
2016-12-14 00:11:41 +0300

    (new) Add extensions for register storage provider

---
+++ JsonFileStorage/JsonFileConfigurationExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace JsonFileStorage
+{
+    using System;
+    using System.Collections.Generic;
+    using Orleans.Runtime.Configuration;
+
+    public static class JsonFileConfigurationExtensions
+    {
+        public static void AddJsonFileStorageProvider(this ClusterConfiguration config, string directory, string providerName = "JsonFileStore")
+        {
+            if (string.IsNullOrWhiteSpace(providerName))
+                throw new ArgumentNullException(nameof(providerName));
+
+            var dictionary = new Dictionary<string, string>
+            {
+                {"directory", directory}
+            };
+
+            config.Globals.RegisterStorageProvider<JsonFileStorageProvider>(providerName, dictionary);
+        }
+    }
+}
