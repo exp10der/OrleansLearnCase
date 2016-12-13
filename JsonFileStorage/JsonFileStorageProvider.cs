8a82f7e38e6d7dfe87dbce20381daa501d0fc4d5
Konstantin <konstantin.writing.code@gmail.com>
2016-12-14 00:00:29 +0300

    (feature) JsonFileStorageProvider

--- JsonFileStorage/JsonFileStorageProvider.cs
+++ JsonFileStorage/JsonFileStorageProvider.cs
@@ -1,7 +1,10 @@
 ﻿namespace JsonFileStorage
 {
     using System;
+    using System.Collections.Generic;
+    using System.IO;
     using System.Threading.Tasks;
+    using Newtonsoft.Json;
     using Orleans;
     using Orleans.Providers;
     using Orleans.Runtime;
@@ -9,33 +12,57 @@
 
     public class JsonFileStorageProvider : IStorageProvider
     {
+        private Func<string, GrainReference, FileInfo> _strategyTakeFileInfo;
+        public string Directory { get; private set; }
+        public string Name { get; set; }
+        public Logger Log { get; }
+
         public Task Init(string name, IProviderRuntime providerRuntime, IProviderConfiguration config)
         {
-            throw new NotImplementedException();
+            Name = name;
+            Directory = config.Properties["directory"];
+
+            _strategyTakeFileInfo =
+                (grainType, grainReference) =>
+                    new FileInfo(Path.Combine(Directory,
+                        string.Format($"{grainType}-{grainReference.ToKeyString()}")));
+
+            return TaskDone.Done;
         }
 
-        public Task Close()
-        {
-            throw new NotImplementedException();
-        }
+        public Task Close() => TaskDone.Done;
 
-        public string Name { get; }
-
-        public Task ReadStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
+        public async Task ReadStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
         {
-            throw new NotImplementedException();
+            var fileInfo = _strategyTakeFileInfo(grainType, grainReference);
+            if (!fileInfo.Exists) return;
+
+            using (var stream = fileInfo.OpenText())
+            {
+                var stringData = await stream.ReadToEndAsync();
+                var json = JsonConvert.DeserializeObject<Dictionary<string, object>>(stringData);
+                grainState.State = json;
+            }
         }
 
         public Task WriteStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
         {
-            throw new NotImplementedException();
+            var json = JsonConvert.SerializeObject(grainState.State);
+            var fileInfo = _strategyTakeFileInfo(grainType, grainReference);
+
+            using (var stream = fileInfo.OpenWrite())
+            using (var writer = new StreamWriter(stream))
+            {
+                return writer.WriteAsync(json);
+            }
         }
 
         public Task ClearStateAsync(string grainType, GrainReference grainReference, IGrainState grainState)
         {
-            throw new NotImplementedException();
-        }
+            var fileInfo = _strategyTakeFileInfo(grainType, grainReference);
+            fileInfo.Delete();
 
-        public Logger Log { get; }
+            return TaskDone.Done;
+        }
     }
 }
